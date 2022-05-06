00770f71078af93dc55c177d656cf936cc333319
guillermo.333 <guillermo.3@hotmail.com.ar>
2022-05-06 16:59:04 +0200

    nav bar and aboutme now responsive

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,15 +4,16 @@ import "./AboutMe.css";
 function AboutMe() {
   return (
     <div className="AboutMeContainer" id="about_me">
-       
-      <div className="AboutMeTexts">   
-        <h3>Let me tell you something about me</h3>
-         <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-         <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
-      </div> 
-      <div className="AboutMePictureDiv">
-        <img src="pp.jpg" className="AboutMePicture" />
-      </div>   
+      <div className='AboutMeContainer2'>
+        <div className="AboutMeTexts">   
+            <h3>Let me tell you something about me</h3>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
+          </div> 
+          <div className="AboutMePictureDiv">
+            <img src="pp.jpg" className="AboutMePicture" />
+          </div>  
+      </div>     
     </div>
   )
 }
